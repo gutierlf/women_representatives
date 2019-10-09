b99e43126d8892029e1541c9cc06e986cdefb68d
Luis F Gutierrez <luis@gutierrezhiller.com>
2019-10-09 11:44:57 -0700

    the version I completed with James during the interview

--- interview_kata.rb
+++ interview_kata.rb
@@ -1,2 +1,45 @@
-class SomeKata
+require 'yaml'
+require 'date'
+
+TERM_YEAR_JANUARY_RULE = lambda do |start_date, end_date|
+  start_year = start_date.year
+  end_year = end_date.year
+
+  end_month = end_date.mon
+  end_year -= 1 if end_month == 1
+  Range.new(start_year, end_year).to_a
 end
+
+def bin_terms(terms, &rule)
+  unless block_given?
+    rule = lambda do |start_date, end_date|
+      Range.new(start_date.year, end_date.year).to_a
+    end
+  end
+  ranges = terms.flat_map do |term|
+    start_date = Date.parse(term["start"])
+    end_date = Date.parse(term["end"])
+    rule.(start_date, end_date)
+  end
+  ranges.reduce(Hash.new(0)) do |acc, cur|
+    acc[cur] += 1
+    acc
+  end
+end
+
+def display_histogram(bins)
+  bins.each do |year, count|
+    bars = "#" * count
+    puts "#{year}: #{bars}"
+  end
+end
+
+data = YAML.load_file('legislators-current.yaml');
+
+females = data.select { |r| r["bio"]["gender"] == "F"}
+terms = females.flat_map do |f|
+  f["terms"].select { |t| t["type"] == "rep" }
+end
+bins = bin_terms(terms)
+display_histogram(bins)
+# build_term_histogram(terms)
--- interview_kata_spec.rb
+++ interview_kata_spec.rb
@@ -1,8 +1,45 @@
 require 'rspec'
 require_relative 'interview_kata'
 
-RSpec.describe SomeKata do
-  it 'can run a test' do
-    expect(subject).to be false
+RSpec.describe "yay" do
+  describe "years rules" do
+    let(:terms) do
+      [
+        {
+          "start" => '1993-01-05',
+          "end" => '1995-01-03'
+        },
+        {
+          "start" => '1995-01-04',
+          "end" => '1997-01-03'
+        },
+      ]
+    end
+
+    context "naive counting, without a rule" do
+      it "builds a hash of years and counts" do
+        expect(bin_terms(terms)).to eq ({
+          1993 => 1,
+          1994 => 1,
+          1995 => 2,
+          1996 => 1,
+          1997 => 1
+        })
+      end
+    end
+
+    context "with our rule" do
+      it "builds a hash of years and counts" do
+        expect(bin_terms(terms, &TERM_YEAR_JANUARY_RULE)).to eq ({
+          1993 => 1,
+          1994 => 1,
+          1995 => 1,
+          1996 => 1,
+        })
+      end
+
+    end
+
+
   end
 end
